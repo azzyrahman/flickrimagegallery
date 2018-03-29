9a2b5f002d8b33d8a81082c530ab95bdcb4417c5
Azizur Rahman <arahman@weatherzone.com.au>
2018-03-30 10:04:19 +1100

    final commit 8

--- data/schema.sql
+++ data/schema.sql
@@ -57,6 +57,26 @@ CREATE TABLE `user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `user_image`
+--
+
+DROP TABLE IF EXISTS `user_image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_image` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `image_id` varchar(45) NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_system` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_index_user_id_image_id` (`user_id`,`image_id`),
+  CONSTRAINT `fk_image_id` FOREIGN KEY (`id`) REFERENCES `flickr_image` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping routines for database 'flickr_images'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -68,5 +88,6 @@ CREATE TABLE `user` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
 
 -- Dump completed on 2018-03-30  8:10:20
